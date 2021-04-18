0267d472f41fe72d4bbefd4c45675bb1f553ede1
hikoand <dorongking2@gmail.com>
2021-04-18 16:02:10 +0900

    add-cost-distance
    add-cost-distance

--- js/common.js
+++ js/common.js
@@ -1,4 +1,7 @@
+let FirstlunchList = ["카레", "우동", "햄버거", "냉면", "마라탕"];
 let lunchList = ["카레", "우동", "햄버거", "냉면", "마라탕"];
+let distanceList = ["10m", "8m", "7m", "7m", "9m"];
+let costList = ["7000won", "5000won", "4500won", "8000won", "9000won"];
 
 function shuffle(a) {
   for (let i = a.length - 1; i > 0; i--) {
@@ -13,7 +16,15 @@ let lunckPick = shuffle(lunchList)[0];
 
 function lunchIs() {
   console.log(lunckPick);
-
   let elem = document.querySelector(".menu_print > h2");
   elem.innerHTML = lunckPick;
+
+  let i = FirstlunchList.indexOf(lunckPick);
+
+  let elem2 = document.querySelector(".main_text> p > span");
+  elem2.innerHTML = distanceList[i];
+
+  let elem3 = document.querySelector("em");
+  console.log(elem3);
+  elem3.innerHTML = costList[i];
 }
